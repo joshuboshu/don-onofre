79503b1bc911c24223959c06c94b6e7ac0b088cd
Joshua <joshua.colman@upacifico.edu.py>
2024-11-09 19:01:25 -0300

    minor fix

--- proyectoweb/settings.py
+++ proyectoweb/settings.py
@@ -125,9 +125,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 STATICFILES_IGNORE_PATTERNS = [
     "proyectowebapp/vendor/font-awesome/less/*.less",
 ]
