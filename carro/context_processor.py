e946b0296c1d01e0db7e28e3e24a31b512559eb2
Joshua <joshua.colman@upacifico.edu.py>
2024-11-14 08:53:37 -0300

    update

--- carro/context_processor.py
+++ carro/context_processor.py
@@ -1,7 +1,8 @@
 def importe_total_carro(request):
     total = 0
     if request.user.is_authenticated:
-        for key, value in request.session["carro"].items():
+        carro = request.session.get("carro", {})
+        for key, value in carro.items():
             total += float(value["precio"])
     else:
         total = "Debes hacer login"
