ca70c865337bbc9fc13527daeef311437bf1ec5c
Joshua <joshua.colman@upacifico.edu.py>
2024-12-14 18:26:08 -0300

    update

--- tienda/urls.py
+++ tienda/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.tienda, name="Tienda"),
+    path('categoria/<int:categoria_id>/', views.productos_por_categoria, name='productos_por_categoria'),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -6,3 +6,9 @@ def tienda(request):
     productos = Producto.objects.all()
     categorias = CategoriaProd.objects.all()  # Obtener todas las categorías
     return render(request, "tienda/tienda.html", {"productos": productos, "categorias": categorias})
+
+
+def productos_por_categoria(request, categoria_id):
+    categoria = CategoriaProd.objects.get(id=categoria_id)
+    productos = Producto.objects.filter(categorias=categoria)
+    return render(request, "carro/widget.html", {"categoria": categoria, "productos": productos})
